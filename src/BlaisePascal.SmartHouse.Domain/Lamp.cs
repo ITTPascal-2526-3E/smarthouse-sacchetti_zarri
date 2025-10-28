41f025b0c737e32c556c40ac6c59ac87b676f9c4
zazaZarri <nikozarro@gmail.com>
2025-10-28 13:56:52 +0100

    Add lamp class

--- src/BlaisePascal.SmartHouse.Domain/Lamp.cs
+++ src/BlaisePascal.SmartHouse.Domain/Lamp.cs
@@ -2,9 +2,37 @@
 {
     public class Lamp
     {
-        int a;
-        int prova = 0;
-        int prova2 = 2;
-        int prova3 = 3;
+        public double brightness {  get; set; } //brightness is in Lumen
+        private double power; //power is in Watt
+        public bool is_on { get; set; } 
+        private string brand;
+        public string lamp_id { get; } //lamp idenficator code
+        public string color { get; set; }
+        
+        public Lamp(double Power,string Brand,string Lamp_id)
+        {
+            if(double.IsPositive(Power))
+            {
+                power = Power;
+            }
+            
+            if(!string.IsNullOrEmpty(Brand))
+            {
+                brand = Brand;
+            }
+            
+            //lamp_id = Lamp_id;
+           
+        }
+        public void turnOn()
+        {
+            is_on=true;
+        }
+
+        public void turnOff()
+        {
+            is_on = false;
+        }
+
     }
 }
