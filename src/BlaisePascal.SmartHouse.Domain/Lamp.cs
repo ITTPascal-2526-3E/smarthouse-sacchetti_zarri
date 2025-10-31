10d187e164bb873fc905609e7f418815a7a802d4
zazaZarri <nikozarro@gmail.com>
2025-10-31 09:35:19 +0100

    added max_brightness and the %

--- src/BlaisePascal.SmartHouse.Domain/Lamp.cs
+++ src/BlaisePascal.SmartHouse.Domain/Lamp.cs
@@ -2,14 +2,15 @@
 {
     public class Lamp
     {
-        public double brightness {  get; set; } //brightness is in Lumen
+        public double max_brightness {  get; set; } //brightness is in Lumen
+        public int brightness_Perc {  get; set; } //bright perc
         private double power; //power is in Watt
         public bool is_on { get; set; } 
         private string brand;
         public string lamp_id { get; } //lamp idenficator code
         public string color { get; set; }
         
-        public Lamp(double Power,string Brand,string Lamp_id)
+        public Lamp(double Power,string Brand,string Lamp_id,double Max_brightness)
         {
             if(double.IsPositive(Power))
             {
@@ -20,17 +21,27 @@
             {
                 brand = Brand;
             }
+
+            if(double.IsPositive(Max_brightness))
+            {
+                max_brightness = Max_brightness;
+            }
+
+            brightness_Perc = 0;
+            is_on = false;
             
             //lamp_id = Lamp_id;
            
         }
         public void turnOn()
         {
-            is_on=true;
+            brightness_Perc = 100;
+            is_on =true;
         }
 
         public void turnOff()
         {
+            brightness_Perc = 0;
             is_on = false;
         }
 
