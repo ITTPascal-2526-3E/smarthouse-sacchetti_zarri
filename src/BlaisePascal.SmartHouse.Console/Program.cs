bfa0eb2d00e8a3c172fac0ba29c243b5813a20fb
“Sacconee” <sacchetti.lorenzo09@icloud.com>
2025-10-31 10:49:34 +0100

    added console tests

--- src/BlaisePascal.SmartHouse.Console/Program.cs
+++ src/BlaisePascal.SmartHouse.Console/Program.cs
@@ -1,10 +1,19 @@
 ﻿
 
-    internal class Program
+using BlaisePascal.SmartHouse.Domain;
+
+public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Lamp lamp = new Lamp(60.0, "Philips", 800.0);
+            lamp.turnOn();
+            lamp.turnOff();
+            Console.WriteLine("Lamp is on: " + lamp.is_on);
+            Console.WriteLine("Lamp power: " + lamp.power + " Watt");
+            Console.WriteLine("Lamp brand: " + lamp.brand);
+            Console.WriteLine("Lamp brightness percentage: " + lamp.brightness_Perc);
+            Console.WriteLine("Lamp max brightness: " + lamp.max_brightness + " Lumen");
         }
     }
 
--- src/BlaisePascal.SmartHouse.Domain/Lamp.cs
+++ src/BlaisePascal.SmartHouse.Domain/Lamp.cs
@@ -4,10 +4,10 @@
     {
         public double max_brightness {  get; set; } //brightness is in Lumen
         public int brightness_Perc {  get; set; } //bright perc
-        private double power; //power is in Watt
-        public bool is_on { get; set; } 
-        private string brand;
-        public string lamp_id { get; set; } //lamp idenficator code (il lamp id verra gestito da una classe esterna AssegnaLampId che controllera la univocità degli lamp_id della casa)
+        public double power; //power is in Watt
+        public bool is_on { get; set; }
+        public string brand { get; }
+        public Guid lamp_Id { get; set; } //lamp idenficator code (il lamp id verra gestito da una classe esterna AssegnaLampId che controllera la univocità degli lamp_id della casa)
         public string color { get; set; }
         
         public Lamp(double Power,string Brand,double Max_brightness)
@@ -36,7 +36,7 @@
         public void turnOn()
         {
             brightness_Perc = 100;
-            is_on =true;
+            is_on = true;
         }
 
         public void turnOff()
@@ -47,3 +47,4 @@
 
     }
 }
+    
