b31baf1d2e7fa0b56b483e9522d0fbd25aee2bbb
“Sacconee” <sacchetti.lorenzo09@icloud.com>
2025-11-11 12:57:15 +0100

    added AirConditioner class with all the console tests and improve program's indentation

--- src/BlaisePascal.SmartHouse.Console/Program.cs
+++ src/BlaisePascal.SmartHouse.Console/Program.cs
@@ -2,51 +2,61 @@
 
 using BlaisePascal.SmartHouse.Domain;
 
-public class Program
+public class Program{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Lamp lamp = new Lamp(60.0, "Philips", 800.0);
-            Lamp lamp2 = new Lamp(60.0, "Philips", 800.0);
-            lamp.turnOn();
-            lamp.turnOff();
-            Console.WriteLine("Lamp is on: " + lamp.is_on);
-            Console.WriteLine("Lamp power: " + lamp.power + " Watt");
-            Console.WriteLine("Lamp brand: " + lamp.brand);
-            Console.WriteLine("Lamp brightness percentage: " + lamp.brightness_Perc);
-            Console.WriteLine("Lamp max brightness: " + lamp.max_brightness + " Lumen");
-            lamp.ChangeColor(LampColor.White);
-            Console.WriteLine("LampColor: " + lamp.Color);
-            lamp.adjustBrightness(23);
-            Console.WriteLine("Lamp brightness percentage: " + lamp.brightness_Perc);
-            Console.WriteLine(lamp.lamp_Id);
-            Console.WriteLine(lamp2.lamp_Id);
+        Lamp lamp = new Lamp(60.0, "Philips", 800.0);
+        Lamp lamp2 = new Lamp(60.0, "Philips", 800.0);
+        lamp.turnOn();
+        lamp.turnOff();
+        Console.WriteLine("Lamp is on: " + lamp.is_on);
+        Console.WriteLine("Lamp power: " + lamp.power + " Watt");
+        Console.WriteLine("Lamp brand: " + lamp.brand);
+        Console.WriteLine("Lamp brightness percentage: " + lamp.brightness_Perc);
+        Console.WriteLine("Lamp max brightness: " + lamp.max_brightness + " Lumen");
+        lamp.ChangeColor(LampColor.White);
+        Console.WriteLine("LampColor: " + lamp.Color);
+        lamp.adjustBrightness(23);
+        Console.WriteLine("Lamp brightness percentage: " + lamp.brightness_Perc);
+        Console.WriteLine(lamp.lamp_Id);
+        Console.WriteLine(lamp2.lamp_Id);
 
-            EcoLamp ecolamp = new EcoLamp(50.0, "Osram", 200.0);
-            ecolamp.turnOn();
-            Console.WriteLine(ecolamp.startTime);
-            Console.WriteLine("EcoLamp is on: " + ecolamp.is_on);
-            Console.WriteLine("EcoLamp power: " + ecolamp.power + " Watt");
-            Console.WriteLine("EcoLamp brand: " + ecolamp.brand);
-            Console.WriteLine("EcoLamp brightness percentage: " + ecolamp.brightness_Perc);
-            Console.WriteLine("EcoLamp max brightness: " + ecolamp.max_brightness + " Lumen");
-            ecolamp.adjustBrightness(99);
-            Console.WriteLine("EcoLamp brightness percentage: " + ecolamp.brightness_Perc);
-            ecolamp.ecoMode();
-            Console.WriteLine("EcoLamp brightness percentage: " + ecolamp.brightness_Perc);
-            ecolamp.turnOff();
+        EcoLamp ecolamp = new EcoLamp(50.0, "Osram", 200.0);
+        ecolamp.turnOn();
+        Console.WriteLine(ecolamp.startTime);
+        Console.WriteLine("EcoLamp is on: " + ecolamp.is_on);
+        Console.WriteLine("EcoLamp power: " + ecolamp.power + " Watt");
+        Console.WriteLine("EcoLamp brand: " + ecolamp.brand);
+        Console.WriteLine("EcoLamp brightness percentage: " + ecolamp.brightness_Perc);
+        Console.WriteLine("EcoLamp max brightness: " + ecolamp.max_brightness + " Lumen");
+        ecolamp.adjustBrightness(99);
+        Console.WriteLine("EcoLamp brightness percentage: " + ecolamp.brightness_Perc);
+        ecolamp.ecoMode();
+        Console.WriteLine("EcoLamp brightness percentage: " + ecolamp.brightness_Perc);
+        ecolamp.turnOff();
 
-            Shutters shutters = new Shutters();
-            ShuttersController autoShutters = new ShuttersController(shutters);
-            autoShutters.Start();
-            Console.WriteLine("la persiana è aperta = ");
-            Console.WriteLine(shutters.is_open);
-            Console.WriteLine("la persiana è chiusa = ");
-            Console.WriteLine(shutters.is_closed);
-
-            var webcam = new Webcam();
-            webcam.Start();
+        Shutters shutters = new Shutters();
+        ShuttersController autoShutters = new ShuttersController(shutters);
+        autoShutters.Start();
+        Console.WriteLine("la persiana è aperta = ");
+        Console.WriteLine(shutters.is_open);
+        Console.WriteLine("la persiana è chiusa = ");
+        Console.WriteLine(shutters.is_closed);
+        //var webcam = new Webcam();
+        // webcam.start();
+        AirConditioner air = new AirConditioner(10.0, 3);
+        air.turnOn();
+        Console.WriteLine(air.air_enabled);
+        Console.WriteLine(air.air_intensity);
+        air.switchIntensity(2);
+        Console.WriteLine(air.air_intensity);
+        air.turnOff();
+        air.turnOn();
+        Console.WriteLine(air.air_intensity);
+        Console.WriteLine(air.lowest_temperature);
+        air.switchTemperature(15.0);
+        Console.WriteLine(air.target_temperature);
 
     }
-    }
+}
 
---
+++ src/BlaisePascal.SmartHouse.Domain/AirConditioner.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain
+{
+    public class AirConditioner
+    {
+        public double lowest_temperature {get; } // temperature is in degrees celsius
+        public int air_intensity { get; set; }
+        public bool air_enabled { get; set; }
+        public int last_air_intensity { get; private set; }
+        public double target_temperature { get; private set;  }
+        public Guid airConditioner_Id { get; set; } = Guid.NewGuid();
+
+        public AirConditioner(double Lowest_temperature, int Air_intensity)
+        {
+            if (Air_intensity > 0 && Air_intensity <= 5)
+            {
+                air_intensity = Air_intensity;
+            }
+
+            lowest_temperature = Lowest_temperature;
+        }
+
+
+        public void turnOn(){
+            air_enabled = true;
+            air_intensity = last_air_intensity;
+        }
+
+        public void turnOff()
+        {
+            air_enabled = false;
+            last_air_intensity = air_intensity;
+        }
+
+        public void switchIntensity(int intensity)
+        {
+            air_intensity = intensity;
+        }
+
+
+        public void switchTemperature(double temperature)
+        {
+            if (temperature > lowest_temperature){
+                target_temperature = temperature;
+            }
+        }
+
+
+    }
+}
