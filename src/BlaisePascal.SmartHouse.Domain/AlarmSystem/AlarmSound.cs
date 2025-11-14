0622c0380e8657142645b67849e1439a0db205d5
“zazaZarri” <nikozarro@gmail.com>
2025-11-14 09:47:30 +0100

    Added SmokeDetector

---
+++ src/BlaisePascal.SmartHouse.Domain/AlarmSystem/AlarmSound.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain
+{
+    public class AlarmSound
+    {
+        public void playAlarm()
+        {
+            Console.WriteLine("Alarm! Smoke detected!");
+        }
+    }
+}
---
+++ src/BlaisePascal.SmartHouse.Domain/AlarmSystem/SmokeDetector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain
+{
+    public class SmokeDetector
+    {
+        public bool smoke_detectet { get; private set; }
+        public bool smoke { get; set; }
+        AlarmSound alarm2 = new AlarmSound();
+
+        public void smokeDetector()
+        {
+            if (smoke == true)
+            {
+                smoke_detectet = true;
+            }
+        }
+
+        public void alarm()
+        {
+            if (smoke_detectet == true)
+            {
+
+                for (int i = 0; i < 2; i++)
+                {
+                    Thread.Sleep(1000); //simulate delay before alarm
+                    alarm2.playAlarm();
+                }
+            }
+
+            smoke_detectet = false;
+        }
+
+    }
+}
