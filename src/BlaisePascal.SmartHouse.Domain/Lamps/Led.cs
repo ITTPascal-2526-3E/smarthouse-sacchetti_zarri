4fdfa11172a2fa60d4163b19a231ad96321e48da
“Sacconee” <sacchetti.lorenzo09@icloud.com>
2025-12-09 13:30:08 +0100

    Added Led class and MatrixLed class, which uses a matrix of Leds

---
+++ src/BlaisePascal.SmartHouse.Domain/Lamps/Led.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain.Lamps
+{
+    public class Led : Device
+    {
+        public double max_brightness { get; protected set; } //brightness is in Lumen
+        public int brightness_Perc { get; protected set; } //bright perc
+        public double power { get; private set; }//power is in Watt
+        public bool is_on { get; protected set; }
+        public string brand { get; protected set; }
+        public LampColor Color { get; protected set; }
+
+
+        public Led(double Power, string Brand, double Max_brightness)
+        {
+            if (double.IsPositive(Power))
+            {
+                power = Power;
+            }
+
+            if (!string.IsNullOrEmpty(Brand))
+            {
+                brand = Brand;
+            }
+
+            if (double.IsPositive(Max_brightness))
+            {
+                max_brightness = Max_brightness;
+            }
+
+            brightness_Perc = 0;
+            is_on = false;
+        }
+
+        public virtual void turnOn()
+        {
+        brightness_Perc = 100;
+        is_on = true;
+        }
+
+        public virtual void turnOff()
+        {
+            brightness_Perc = 0;
+            is_on = false;
+        }
+
+        public void adjustBrightness(int new_bright_perc)
+        {
+            if (int.IsPositive(new_bright_perc) && new_bright_perc <= 100)
+            {
+                brightness_Perc = new_bright_perc;
+            }
+            else
+                throw new ArgumentException("Brightness percentage must be between 0 and 100.");
+        }
+
+    }
+}
---
+++ src/BlaisePascal.SmartHouse.Domain/Lamps/MatrixLed.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain.Lamps
+{
+    public class MatrixLed : Device
+    {
+        Led[,] matrix;
+
+        public void GenerateMatrix(int rows, int columns, Led led) 
+        {    
+            matrix = new Led[rows, columns];
+            
+            for(int i = 0;  i < rows; i++)
+            {
+                for(int j = 0;  j < columns; j++)
+                {
+                    matrix[i, j] = led;
+                }
+            }
+        }
+
+        public void SwitchOnAll()
+        {
+            for(int i = 0;i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0;j < matrix.GetLength(1); j++)
+                {
+                    matrix[i,j].turnOn();
+                }
+            }
+        }
+
+        public void SwitchOffAll()
+        {
+            for (int i = 0; ; i++)
+            {
+                for (int j = 0; j <= matrix.GetLength(2); j++)
+                {
+                    matrix[i,j].turnOff();
+                }
+            }
+        }
+
+        public void SetIntensityAll(int intensity)
+        {
+            for (int i = 0; ; i++)
+            {
+                for (int j = 0; j <= matrix.GetLength(2); j++)
+                {
+                    matrix[i, j].adjustBrightness(intensity);
+                }
+            }
+        }
+
+        public void PatternCheckerBoard()
+        {
+            for (int i = 0; ; i++)
+            {
+                for (int j = 0; j <= matrix.GetLength(2); j++)
+                {
+                    matrix[i, j].turnOn();
+                }
+            }
+
+            if (matrix.GetLength(1) % 2 == 0)
+            {
+                for (int i = 0; ; i++)
+                {
+                    for (int j = 0; j <= matrix.GetLength(2); j++)
+                    {
+                        if (j % 2 == 0)
+                            matrix[i, j].turnOff();
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; ; i++)
+                {
+                    for (int j = 0; j <= matrix.GetLength(2); j++)
+                    {
+                        if ((i % 2) == (j % 2))
+                        {
+                            matrix[i, j].turnOn();
+                        }
+                         
+                    }
+                }
+            }
+
+        }
+
+        public Led GetLed(int rows, int columns)
+        {
+           return matrix[rows, columns];
+        }
+    }
+}
