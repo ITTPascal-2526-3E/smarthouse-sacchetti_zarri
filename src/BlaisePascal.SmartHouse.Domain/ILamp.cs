036c68dd33538159dabe72d69566dc0689cfe20c
“Sacconee” <sacchetti.lorenzo09@icloud.com>
2026-01-16 10:51:44 +0100

    added a first and partial implementation of ILamp interface that needs to be completed

---
+++ src/BlaisePascal.SmartHouse.Domain/ILamp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain
+{
+    public interface ILamp
+    {
+        public void adjustBrightness(int new_bright_perc);
+    }
+}
