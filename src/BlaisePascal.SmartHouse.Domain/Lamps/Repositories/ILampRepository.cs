e0c789eacbfd2f74ae34c52ad3e5e3c95a6516cb
“zazaZarri” <nikozarro@gmail.com>
2026-02-06 10:58:09 +0100

    Added repository

---
+++ src/BlaisePascal.SmartHouse.Domain/Lamps/Repositories/ILampRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain.Lamps.Repositories
+{
+    public interface ILampRepository
+    {
+        void Add(Lamp lamp);
+        void Update(Lamp lamp);
+        void Remove(Guid id);
+        Lamp GetById(Guid id);
+        List<Lamp> GetAll();
+
+    }
+}
