2c289b154ba08350cfd05fe498f590570ef707bf
“zazaZarri” <nikozarro@gmail.com>
2026-02-17 15:44:48 +0100

    Fixed Application

---
+++ src/BlaisePascal.SmartHouse.Application/Devices/Lamps/Command/RemoveLampCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Application.Devices.Lamps.Command
+{
+    internal class RemoveLampCommand
+    {
+    }
+}
